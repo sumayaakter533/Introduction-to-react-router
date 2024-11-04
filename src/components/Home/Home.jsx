097d86208c6d8e4045fed9e1670eada6b060841b
sumayaakter533 <sumaya55230@gmail.com>
2024-11-04 07:53:17 +0600

    data loading added using useNavigation()

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Hero from "../Hero/Hero";
 
 const Home = () => {
     const location = useLocation();
+    const navigation = useNavigation();
 
     return (
         <div className='flex flex-col min-h-screen'>
             <Header></Header>
             {location.pathname === "/" && <Hero></Hero>}
-            <div className='flex-grow'>
-                <Outlet></Outlet>
-            </div>
+            {navigation.state === "loading" ? (
+                <p className='text-center text-4xl py-10'>Loading...</p>
+            ) : (
+                <div className='flex-grow'>
+                    <Outlet></Outlet>
+                </div>
+            )}
+
             <Footer></Footer>
         </div>
     );
